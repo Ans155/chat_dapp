e9727d568b0b390a22be93ca56fde5f04fc0b36c
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-06-02 19:01:06 +0530

    app_structure

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import navbar from './Navbar/navbar';
+import userCard from './UserCard/userCard';
+import model from './Model/model';
+import loader from'./Loader/loader';
+import friend from './Friend/friend';
+import filter from './Filter/filter';
+import error from './Error/error';
+
+
+export { navbar, userCard, model, loader, friend, filter, error};
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const MyApp = ({ Component, pageProps}) => (
+  
+    <div>
+        <Component {...pageProps} />
+    </div>
+  );
+
+
+export default MyApp
