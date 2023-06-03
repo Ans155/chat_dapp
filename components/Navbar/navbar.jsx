47e6c1e018e94e12e268073a3c62fb029c74b682
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-06-03 16:48:52 +0530

    frontend_setup

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useEffect, useState, useContext} from 'react'
+import Image from 'next/image';
+import Link from "next/link";
+
+import Style from "./navbar.module.css";
+import { AppContext } from '../../context/AppContext';
+import { model, error } from '../index';
 
 const navbar = () => {
   return (
